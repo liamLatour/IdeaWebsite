53f2953b68484caef95e50264237415f2df14b20
liamLatour <38808200+liamLatour@users.noreply.github.com>
2018-05-08 07:19:18 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8" />
+    <title>Une idée?</title>
+    <style>
+    body {
+        margin: 0px;
+    }
+
+    div.content {
+        margin-left: 25%;
+        padding: 1px 16px;
+        height: 1000px;
+        margin-top: 20px;
+    }
+
+    textarea {
+        resize: none;
+        text-align: left;
+        padding: 10px;
+        width: 100%;
+        height: 50px;
+        background-color: rgb(238, 238, 238);
+        border: none;
+    }
+    a.button {
+        background-color: rgb(238, 238, 238);
+
+        text-decoration: none;
+        color: initial;
+        padding: 10px 10px;
+    }
+    </style>
+</head>
+<body>
+<!--Menu-->
+<?php include("menu.php"); ?>
+
+<!--Input-->
+<div class="content" align="center">
+<a href="http://google.com" class="button">Post an Idea</a>
+</div>
+</body>
+</html>

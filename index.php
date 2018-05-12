2e313a4c393921303b3fbc7f7f619d7da49a0573
liamLatour <38808200+liamLatour@users.noreply.github.com>
2018-05-12 17:35:01 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+
+switch ($lang){
+    case "fr":
+        header('Location: fr/index.php');
+        break;
+    case "en":
+        header('Location: en/index.php');
+        break;
+    default:
+        header('Location: en/index.php');
+        break;
+}
+?>
